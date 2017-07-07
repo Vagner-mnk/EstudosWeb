5deab5d1020c7ca21cbd8787db87c21d9e8bba0c
vagnermnk <vagner.kobayakawa@fatec.sp.gov.br>
2017-07-06 22:54:12 -0300

    06-07-2017 Casa

--- Modelo/js/script.js
+++ Modelo/js/script.js
@@ -1,1 +1,21 @@
- 
+/*var result = confirm("Deseja excluir");*/
+
+console.log(confirm("Olá"));
+
+var a=10;
+var b = 20;
+
+
+function somar(a, b){
+	return a + b;
+}
+
+//alert(somar(a,b));
+
+
+document.getElementById("btn_calcular").onclick = function(){
+	var valorA = document.getElementById("valor_a").value;
+	var valorB = document.getElementById("valor_b").value;
+	alert(somar(parseInt(valorA), parseInt(valorB)));
+	//console.log(parseInt(valorA) + parseInt(valorB));
+}
